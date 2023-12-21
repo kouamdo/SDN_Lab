0b52d537d6fd61ca0a7b24cef0df8688ce996048
kouamdo <kouamdux@gmail.com>
2023-12-21 12:31:33 +0100

    add acl rule

--- acl.tf
+++ acl.tf
@@ -1,123 +1,136 @@
+# --- Pub acl rules ---
+resource "aws_network_acl_rule" "pub_sub_ssh_rule" {
+    network_acl_id = aws_network_acl.pub_sub_acl.id
+    rule_number    = 200
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = false
+    cidr_block     = "5.161.73.127/32"
+    from_port      = 22
+    to_port        = 22
+}
+
+resource "aws_network_acl_rule" "pub_sub_internet_rule" {
+    network_acl_id = aws_network_acl.pub_sub_acl.id
+    rule_number    = 300
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = true
+    cidr_block     = "0.0.0.0/0"
+    from_port      = 0
+    to_port        = 65535
+}
 resource "aws_network_acl" "pub_sub_acl" {
     vpc_id = aws_vpc.core5g_vpc.id
-    subnet_ids = [ aws_subnet.pub_subnet.id ]
-
-    ingress {
-            rule_no = 200
-            to_port    = 22
-            from_port = 22
-            protocol = "tcp"
-            action     = "allow"
-            cidr_block = "0.0.0.0/0"
-        }
-    ingress {
-            protocol = "tcp"
-            rule_no = 300
-            from_port = 0
-            to_port    = 6553
-            action     = "allow"
-            cidr_block = "0.0.0.0/0"
-        }
-
-    egress {
-        protocol   = "tcp"
-        rule_no    = 300
-        from_port  = 0
-        to_port    = 65535
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
-    egress {
-        protocol   = "tcp"
-        rule_no    = 200
-        from_port  = 80
-        to_port    = 80
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-        }
+    subnet_ids = [ aws_subnet.pub_subnet.id]
     tags = {
     Name = "pub_sub_acl"
   }
 }
 
+# --- ---
+
+
+
+# --- k8s acl rules ---
+
+resource "aws_network_acl_rule" "k8s_sub_internet_rule" {
+    network_acl_id = aws_network_acl.k8s_sub_acl.id
+    rule_number    = 300
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = true
+    cidr_block     = "0.0.0.0/0"
+    from_port      = 0
+    to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "k8s_sub_ssh_rule" {
+    network_acl_id = aws_network_acl.k8s_sub_acl.id
+    rule_number    = 200
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = false
+    cidr_block     = var.subnet_pub
+    from_port      = 22
+    to_port        = 22
+}
+
 resource "aws_network_acl" "k8s_sub_acl" {
     vpc_id = aws_vpc.core5g_vpc.id
     subnet_ids = [ aws_subnet.subnet_k8s_1.id ]
-
-    ingress {
-        protocol = "tcp"
-        rule_no = 300
-        from_port = 0
-        to_port    = 6553
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
-
-
-    egress {
-        protocol   = "tcp"
-        rule_no    = 300
-        from_port  = 0
-        to_port    = 65535
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
     tags = {
     Name = "k8s_sub_acl"
   }
+}
+
+# --- ---
+
+
+# --- dp acl rules ---
+resource "aws_network_acl_rule" "dp_sub_internet_rule" {
+    network_acl_id = aws_network_acl.dp_sub_acl.id
+    rule_number    = 300
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = true
+    cidr_block     = "0.0.0.0/0"
+    from_port      = 0
+    to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "dp_sub_ssh_rule" {
+    network_acl_id = aws_network_acl.dp_sub_acl.id
+    rule_number    = 200
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = false
+    cidr_block     = var.subnet_pub
+    from_port      = 22
+    to_port        = 22
 }
 
 resource "aws_network_acl" "dp_sub_acl" {
     vpc_id = aws_vpc.core5g_vpc.id
     subnet_ids = [ aws_subnet.subnet_dp_1.id , aws_subnet.subnet_dp_2.id ]
 
-    ingress {
-        protocol = "tcp"
-        rule_no = 300
-        from_port = 0
-        to_port    = 6553
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
-
-
-    egress {
-        protocol   = "tcp"
-        rule_no    = 300
-        from_port  = 0
-        to_port    = 65535
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
     tags = {
     Name = "dp_sub_acl"
   }
 }
 
+# --- ---
+
+# --- sig acl rules ---
+
+resource "aws_network_acl_rule" "sig_sub_internet_rule" {
+    network_acl_id = aws_network_acl.sig_sub_acl.id
+    rule_number    = 300
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = true
+    cidr_block     = "0.0.0.0/0"
+    from_port      = 0
+    to_port        = 65535
+}
+
+resource "aws_network_acl_rule" "sig_sub_ssh_rule" {
+    network_acl_id = aws_network_acl.sig_sub_acl.id
+    rule_number    = 200
+    protocol       = "tcp"
+    rule_action    = "allow"
+    egress         = false
+    cidr_block     = var.subnet_pub
+    from_port      = 22
+    to_port        = 22
+}
 resource "aws_network_acl" "sig_sub_acl" {
     vpc_id = aws_vpc.core5g_vpc.id
 
     subnet_ids = [ aws_subnet.subnet_sig_1.id , aws_subnet.subnet_sig_2.id ]
-
-    ingress {
-        protocol = "tcp"
-        rule_no = 300
-        from_port = 0
-        to_port    = 6553
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
-
-
-    egress {
-        protocol   = "tcp"
-        rule_no    = 300
-        from_port  = 0
-        to_port    = 65535
-        action     = "allow"
-        cidr_block = "0.0.0.0/0"
-    }
     tags = {
     Name = "sig_sub_acl"
   }
 }
+
+# --- ---
