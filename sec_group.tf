623895d55659ca3a6a04b274634e2a7fdd14b6f1
kouamdo <kouamdux@gmail.com>
2023-12-21 18:16:49 +0100

    add security group

--- sec_group.tf
+++ sec_group.tf
@@ -1,3 +1,60 @@
-resource "aws_security_group" "sec_group_bastion" {
-    
+resource "aws_security_group" "pub_sec_group" {
+  name        = "allow-ssh-and-internet"
+  description = "Allow SSH and Internet access inside host in pub subnet"
+  vpc_id      = aws_vpc.core5g_vpc.id
+
+  // Règle d'entrée pour la communication SSH à partir de 5.167.22.7
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["5.161.73.127/32"]
+  }
+
+  // Règle d'entrée pour la communication sur Internet
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Règle de sortie autorisant tout le trafic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "priv_sec_group" {
+  name        = "allow-ssh-and-internet"
+  description = "Allow SSH and Internet access"
+  vpc_id      = aws_vpc.core5g_vpc.id
+
+  // Règle d'entrée pour la communication SSH à partir de 5.167.22.7
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.subnet_pub]
+  }
+
+  // Règle d'entrée pour la communication sur Internet
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Règle de sortie autorisant tout le trafic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
